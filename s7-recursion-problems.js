5fc4bd39d80963adeaed32678bb2c94803895ce4
Mark Janssen-vooles <m.janssen-vooles@outlook.com.au>
2023-07-19 12:00:28 +1000

    Solve factorial using helper method recursion

--- s7-recursion-problems.js
+++ s7-recursion-problems.js
@@ -25,3 +25,32 @@ const power = (base, exponent) => {
 
 // =================
 
+// factorial
+// Write a function factorial which accepts a number and returns the factorial of that number. A factorial is the product of an integer and all the integers below it; e.g., factorial four ( 4! ) is equal to 24, because 4 * 3 * 2 * 1 equals 24.  factorial zero (0!) is always 1.
+
+const factorial = (num) => {
+    if (num === 0) return 1 
+    if (num === 1) {
+      return num
+    }
+
+    let result = num
+
+    const helper = (input) => {
+      if (input === 1) {
+        return 
+      }
+      result = result * input
+      helper(input - 1)
+    }
+    helper(num - 1)
+
+    return result
+}
+
+// console.log(factorial(1)) // 1
+// console.log(factorial(2)) // 2
+// console.log(factorial(4)) // 24
+// console.log(factorial(7)) // 5040
+
+// ==================
