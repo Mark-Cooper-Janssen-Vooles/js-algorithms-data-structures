a2d8a1a7a68f91f82d0bc88bd9db3c0424ce09e3
Mark Janssen-vooles <m.janssen-vooles@outlook.com.au>
2023-07-19 16:35:09 +1000

    Flatten problem

--- s7-recursion-problems-hard.js
+++ s7-recursion-problems-hard.js
@@ -88,7 +88,35 @@ const someRecursive = (arr, callback) => {
 // SAMPLE INPUT / OUTPUT
 const isOdd = val => val % 2 !== 0;
 
-console.log(someRecursive([1,2,3,4], isOdd)) // true
-console.log(someRecursive([4,6,8,9], isOdd)) // true
-console.log(someRecursive([4,6,8], isOdd)) // false
-console.log(someRecursive([4,6,8], val => val > 10)); // false
+// console.log(someRecursive([1,2,3,4], isOdd)) // true
+// console.log(someRecursive([4,6,8,9], isOdd)) // true
+// console.log(someRecursive([4,6,8], isOdd)) // false
+// console.log(someRecursive([4,6,8], val => val > 10)); // false
+
+// ===============
+
+// // flatten
+// // Write a recursive function called flatten which accepts an array of arrays and returns a new array with all values flattened.
+
+const flatten = (arr) => {
+  let result = []
+
+  for (let i = 0; i < arr.length; i ++) {
+    if (Array.isArray(arr[i])) {
+      result = result.concat(flatten(arr[i]))
+    } else {
+      result.push(arr[i])
+    }
+  }
+
+  return result
+}
+
+console.log(flatten([1, [2, [3, 4], [5]] ])) // [1, 2, 3, 4, 5]
+console.log(flatten([1, 2, 3, [4, 5] ])) // [1, 2, 3, 4, 5]
+console.log(flatten([1, [2, [3, 4], [[5]]]])) // [1, 2, 3, 4, 5]
+console.log(flatten([[1],[2],[3]])) // [1,2,3]
+console.log(flatten([[[[1], [[[2]]], [[[[[[[3]]]]]]]]]])) // [1,2,3]
+
+// =======
+
