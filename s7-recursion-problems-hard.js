c6c525a20554bd92952cf120e6529bf158966528
Mark Janssen-vooles <m.janssen-vooles@outlook.com.au>
2023-07-19 15:20:18 +1000

    do someRecursive problem

--- s7-recursion-problems-hard.js
+++ s7-recursion-problems-hard.js
@@ -27,7 +27,7 @@ const reverse = (str) => {
 
 const isPalindrome = (str) => {
   let result = true
-  
+
   const helper = (val) => {
     // if 0 length or 1 or false result, return 
     if (val.length === 0 || val.length === 1 || result === false) return
@@ -52,8 +52,43 @@ const isPalindrome = (str) => {
   return result
 }
 
-console.log(isPalindrome('awesome')) // false
-console.log(isPalindrome('foobar')) // false
-console.log(isPalindrome('tacocat')) // true
-console.log(isPalindrome('amanaplanacanalpanama')) // true
-console.log(isPalindrome('amanaplanacanalpandemonium')) // false
+// console.log(isPalindrome('awesome')) // false
+// console.log(isPalindrome('foobar')) // false
+// console.log(isPalindrome('tacocat')) // true
+// console.log(isPalindrome('amanaplanacanalpanama')) // true
+// console.log(isPalindrome('amanaplanacanalpandemonium')) // false
+
+// ================
+
+
+// someRecursive
+// Write a recursive function called someRecursive which accepts an array and a callback. The function returns true if a single value in the array returns true when passed to the callback. Otherwise it returns false.
+
+const someRecursive = (arr, callback) => {
+  let result = false
+
+  const helper = (val) => {
+    // if arr length === 0, return 
+    if (val.length === 0) return
+    // pass value to callback
+      // if true, set result to true and exit
+      // if false, call helper again with first value removed
+    if (callback(val[0]) === true) {
+      result = true
+      return
+    }
+
+    helper(val.slice(1))
+  }
+  helper(arr)
+
+  return result
+}
+
+// SAMPLE INPUT / OUTPUT
+const isOdd = val => val % 2 !== 0;
+
+console.log(someRecursive([1,2,3,4], isOdd)) // true
+console.log(someRecursive([4,6,8,9], isOdd)) // true
+console.log(someRecursive([4,6,8], isOdd)) // false
+console.log(someRecursive([4,6,8], val => val > 10)); // false
