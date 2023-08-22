128d5dfcb9fb13d4850f14c621fdd7227cbb2859
Mark Janssen-vooles <m.janssen-vooles@outlook.com.au>
2023-08-22 17:24:33 +1000

    insert function for BST using recursion

--- s22-binary-search-trees.js
+++ s22-binary-search-trees.js
@@ -10,6 +10,35 @@ class BinarySearchTree {
   constructor() {
     this.root = null
   }
+
+  insert(val) {
+    const newNode = new Node(val)
+    if (this.root === null) {
+      this.root = newNode
+      return newNode
+    } 
+
+    const recursivelyCheck = (comparisonNode) => {
+      if (newNode.value > comparisonNode.value) {
+        // insert to the right, or recursively call 
+        if (comparisonNode.right === null) {
+          comparisonNode.right = newNode
+        } else {
+          recursivelyCheck(comparisonNode.right)
+        }
+      } else {
+        // insert to the left, or recursively call
+        if (comparisonNode.left === null) {
+          comparisonNode.left = newNode
+        } else {
+          recursivelyCheck(comparisonNode.left)
+        }
+      }
+    }
+
+    recursivelyCheck(this.root)
+    return newNode
+  }
 }
 
 const tree = new BinarySearchTree()
@@ -19,3 +48,12 @@ tree.root.left = new Node(7)
 tree.root.left.right = new Node(8)
 
 console.log(JSON.stringify(tree))
+
+const tree1 = new BinarySearchTree()
+
+tree1.insert(10)
+tree1.insert(15)
+tree1.insert(7)
+tree1.insert(8)
+
+console.log(JSON.stringify(tree1))
